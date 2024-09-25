64038355b430030cd19038ab8e46fc4e919a3512
SArcD <141703135+SArcD@users.noreply.github.com>
2024-09-25 11:45:38 -0600

    Update main.py

--- main.py
+++ main.py
@@ -415,7 +415,7 @@ df_supernovas_clustering = df_parametros.dropna()
 
 # Seleccionar las columnas numéricas para el clustering
 #columnas_numericas = df_supernovas_clustering.select_dtypes(include=['number'])
-columnas_numericas = df_supernovas_clustering.select_dtypes(include=['number']).drop(columns=['RA', 'Dec'])
+columnas_numericas = df_supernovas_clustering.select_dtypes(include=['number']))
 st.write(df_supernovas_clustering)
 
 
