1b70a73224239d352aff316b7c83f3fbc67b1406
SArcD <141703135+SArcD@users.noreply.github.com>
2024-09-27 15:21:44 -0600

    Update main.py

--- main.py
+++ main.py
@@ -653,7 +653,7 @@ if len(supernovae_in_cluster) > 0:
 else:
     st.write(f"No supernovae in {selected_cluster}.") 
 
-st.write(df_supernova_clustering.columns)
+st.write(df_supernova_clustering)
 
 ########
 import pandas as pd
