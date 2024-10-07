44eeb9e9eedefc864e2c448792a1ae796b3f67c8
SArcD <141703135+SArcD@users.noreply.github.com>
2024-10-06 20:41:36 -0600

    Update main.py

--- main.py
+++ main.py
@@ -2808,7 +2808,7 @@ fig_hist.update_layout(
     xaxis_title='MJD (Modified Julian Date)',
     yaxis_title='Cantidad de Neutrinos que Llegaron a la Tierra',
     xaxis=dict(range=[mjd_min, mjd_max]),  # Limitar el eje x a los valores de MJD
-    yaxis=dict(type='log', title='Cantidad de Neutrinos (Escala Logarítmica)'),  # Eje y logarítmico
+    #yaxis=dict(type='log', title='Cantidad de Neutrinos (Escala Logarítmica)'),  # Eje y logarítmico
     barmode='group'
 )
 
